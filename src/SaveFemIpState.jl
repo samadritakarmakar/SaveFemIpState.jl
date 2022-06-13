10d97a88522c5cfb610ab5cc125c8c17bedf8fab
Samadrita Karmakar <samadritakarmakar@gmail.com>
2022-06-13 17:15:04 +0200

    trial 2: added a new type ipState to simplify saving of integration point data by element and integration point number

--- src/SaveFemIpState.jl
+++ src/SaveFemIpState.jl
@@ -3,11 +3,15 @@ module SaveFemIpState
 include("state.jl")
 include("ipState.jl")
 
+
 AbstractIpState = Union{Dict{Tuple{Int64, Int64}, IpStateSingle}, 
 Dict{Tuple{Int64, Int64}, IpStateArray}, IpState}
 
+#from state.jl
 export AbstractIpState, IpStateSingle, IpStateArray
-
+#from ipState.jl
+export IpState
+#from common
 export getIpState!, getIpState, createIpStateDict, updateIpStateDict!, updateIpStateDict4rmBuffer!, updateStateDict!
 
 end # module
